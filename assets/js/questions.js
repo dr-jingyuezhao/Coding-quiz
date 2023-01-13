e0174e48e2be52c4857a5719f983f564104f69c7
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-13 19:33:33 +0000

    create a list of 5 coding quiz questions in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+// list the 5 quiz questions by defining objects key-value pairs
+var quizQuestions = [
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: [
+            "A. var",
+            "B. let",
+            "C. Both A and B",
+            "D. None of the above"
+        ],
+        correctAnswerIndex: 2 
+    }
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        answers: [
+            "A. in",
+            "B. is in",
+            "C. exists",
+            "D. lies"
+        ],
+        correctAnswerIndex: 0 
+    }
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        answers: [
+            "A. stringify()",
+            "B. parse()",
+            "C. convert()",
+            "D. None of the above"
+        ],
+        correctAnswerIndex: 0 
+    }
+    {
+        question: "How to stop an interval timer in Javascript?",
+        answers: [
+            "A. clearTimer",
+            "B. clearInterval",
+            "C. intervalOver",
+            "D. None of the above"
+        ],
+        correctAnswerIndex: 1 
+    }
+    {
+        question: "What is the process of finding errors and fixing them within a program.",
+        answers: [
+            "A. Compiling",
+            "B. Executing",
+            "C. Scanning",
+            "D. Debugging"
+        ],
+        correctAnswerIndex: 3 
+    }
+];
