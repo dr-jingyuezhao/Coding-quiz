80963e0521ad9b10b04438573a1b02470e13eb99
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-16 13:09:13 +0000

    add score and print to the console

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,13 +6,17 @@ var startButton = document.querySelector("#start");
 var timeEl = document.querySelector("#time");
 var timeLeft = 90;
 var subtractTime = 10;
-var chosenAnswer = "";
-var numBlanks = 0;
-var correctCounter = 0;
-var wrongCounter = 0;
+var score = 0;
+var winScore = 5;
+var finalScore;
 var isCorrect = true;
 var isWrong = true;
 
+var startScreen = document.getElementById("start-screen");
+var questionsDiv = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var choices = document.getElementById("choices");
+var feedback = document.getElementById("feedback");
 
 // WHEN I click the start button
 // The startQuiz function is called when the start button is clicked
@@ -44,48 +48,55 @@ var q = quizQuestions[questionIndex];
 // and I am presented with a question
 function renderQuestion() {
   // hide the start screen with rules
-  var startScreen = document.getElementById("start-screen");
   startScreen.style.display = "none";
   // show questions on the screen
-  var questionsDiv = document.getElementById("questions");
   questionsDiv.style.display = "block";
   // Creates a question on screen
-  var questionTitle = document.getElementById("question-title");
   questionTitle.textContent = q.question;
-  var choices = document.getElementById("choices");
   // Create a new button for each choice (answer)
   for (var i = 0; i < q.answers.length; i++) {
     var choiceButton = document.createElement("button");
     choiceButton.textContent = q.answers[i];
     choiceButton.setAttribute("data-index", i);
     choices.appendChild(choiceButton);
-
     // WHEN I answer a question, add a click event and check if the answer is correct
     choiceButton.addEventListener("click", checkAnswer);
   }
 
 }
 
+// function showNextQuestion() {
+//     // WHEN I answer a question, add a click event and check if the answer is correct
+//     var choice = document.querySelectorAll("button");
+//     choice.textContent = 
+//     choiceButton.addEventListener("click", checkAnswer);
+
+
+// }
+
 function checkAnswer(event) {
   event.preventDefault();
   var answer = event.currentTarget.dataset.index;
   var correctAnswer;
-  var feedback = document.getElementById("feedback");
   feedback.style.display = "block";
   if (answer == q.correctAnswerIndex) {
     correctAnswer = answer;
     // If the answer is correct, display feedback to be Correct!
     feedback.textContent = "Correct!";
+    score = score + winScore;
   } else {
     // When I answer a question incorrectly
     feedback.textContent = "Wrong!";
+    score += 0;
     // Then time is subtracted from the clock
     timeLeft -= subtractTime;
     if (timeLeft < 0) {
       timeLeft = 0;
     }
   }
+  console.log(score);
   questionIndex += 1;
+  // showNextQuestion();
 }
 // THEN I am presented with another question
 // WHEN all questions are answered or the timer reaches 0
