95e1fce4eb79ef4c7e998941c3cc7d92ed62572b
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-16 17:18:43 +0000

    add endQuiz function and change the variable value in the checkanswer and renderQuestion function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,8 +4,10 @@
 // declare variables
 var startButton = document.querySelector("#start");
 var timeEl = document.querySelector("#time");
-var timeLeft = 60;
+var timeLeft = 10;
 var subtractTime = 10;
+var questionIndex = 0;
+
 var score = 0;
 var winScore = 5;
 var lastUserScore;
@@ -15,10 +17,15 @@ var questionsDiv = document.getElementById("questions");
 var questionTitle = document.getElementById("question-title");
 var choices = document.getElementById("choices");
 var feedback = document.getElementById("feedback");
+var endScreen = document.getElementById("end-screen");
 
 // WHEN I click the start button
 // The startQuiz function is called when the start button is clicked
 function startQuiz() {
+  // hide the start screen with rules
+  startScreen.style.display = "none";
+  // show questions on the screen
+  questionsDiv.classList.remove("hide");
   // THEN a timer starts 
   startTimer();
   renderQuestion();
@@ -37,20 +44,20 @@ function startTimer() {
     if (timeLeft === 0) {
       // Clears interval
       clearInterval(timer);
+      endQuiz();
     }
   }, 1000);
 }
 
-var questionIndex = 0;
 var q = quizQuestions[questionIndex];
 // and I am presented with a question
 function renderQuestion() {
-  // hide the start screen with rules
-  startScreen.style.display = "none";
-  // show questions on the screen
-  questionsDiv.style.display = "block";
   // Creates a question on screen
+  var q = quizQuestions[questionIndex];
+  if (timeLeft > 0 || questionIndex <= quizQuestions.length - 1) {
   questionTitle.textContent = q.question;
+  choices.textContent = "";
+
   // Create a new button for each choice (answer)
   for (var i = 0; i < q.answers.length; i++) {
     var choiceButton = document.createElement("button");
@@ -59,9 +66,9 @@ function renderQuestion() {
     choices.appendChild(choiceButton);
     // WHEN I answer a question, add a click event and check if the answer is correct
     choiceButton.addEventListener("click", checkAnswer);
-    questionIndex += 1;
 
   }
+}
 
 }
 
@@ -76,6 +83,7 @@ function renderQuestion() {
 
 function checkAnswer(event) {
   event.preventDefault();
+  var q = quizQuestions[questionIndex];
   var answer = event.currentTarget.dataset.index;
   var correctAnswer;
   feedback.style.display = "block";
@@ -90,20 +98,31 @@ function checkAnswer(event) {
     score += 0;
     // Then time is subtracted from the clock
     timeLeft -= subtractTime;
-    if (timeLeft < 0) {
+    if (timeLeft <= 0 || questionIndex === quizQuestions.length - 1) {
       timeLeft = 0;
+      endQuiz();
+      return;
     }
   }
+  questionIndex++;
+  renderQuestion();
+
   console.log(score);
-  function storeScores() {
-    // Stringify and set "score" key in localStorage to score variable
-    localStorage.setItem("score", score.toString());
-    lastUserScore = localStorage.getItem("score");
-  }
-  console.log(lastUserScore);
-  // questionIndex += 1;
-  // showNextQuestion();
+  // function storeScores() {
+  //   // Stringify and set "score" key in localStorage to score variable
+  //   localStorage.setItem("score", score.toString());
+  //   lastUserScore = localStorage.getItem("score");
+  // }
+  // console.log(lastUserScore);
+  // // questionIndex += 1;
+  // // showNextQuestion();
 }
 // THEN I am presented with another question // this doesn't work
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
+function endQuiz() {
+  console.log("Ending quiz")
+  endScreen.classList.remove("hide");
+  questionsDiv.classList.add("hide");
+  timeLeft = 0;
+}
